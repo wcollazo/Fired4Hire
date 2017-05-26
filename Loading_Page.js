448aba76f4645f67715a7bcfaad4cdea38538f77
William Collazo <wcollazo@cristoreyny.org>
2017-05-26 13:40:32 +0000

    misson complete

--- Loading_Page.js
+++ Loading_Page.js
@@ -1,5 +1,14 @@
 // JavaScript File
  
- $("html").setTimeout(function(){ 
+ if (1 === 1) {  $("h1").html("Confirming with the Job...")
+    
+
+    setTimeout(function(){ 
         window.location='Final_Page.html'
     }, 5000);
+    
+    
+ }
+    
+    
+  
